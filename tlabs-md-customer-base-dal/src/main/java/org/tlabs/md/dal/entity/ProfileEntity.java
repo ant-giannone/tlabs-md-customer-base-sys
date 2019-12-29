cf4bb7ac1e703d6a6db0c00b01c404bc8f62dec3
Antonino Giannone <antonino.giannone@gmail.com>
2019-12-29 20:42:22 +0100

    spring - hibernate/JPA - hikari-cp support

---
+++ tlabs-md-customer-base-dal/src/main/java/org/tlabs/md/dal/entity/ProfileEntity.java
@@ -0,0 +1,44 @@
+package org.tlabs.md.dal.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "profiles")
+public class ProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
